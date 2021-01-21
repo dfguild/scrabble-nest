4aa8f9a5faa2bc1292f07446b5bf93af4df0fafb
dfguild <dfguild1@gmail.com>
2021-01-21 11:18:09 -0500

    added static serving for debug

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 import { GameModule } from './game/game.module';
 import { CoreModule } from './core/core.module';
 import { AppLoggerMiddleware } from './core/app-logger.middleware';
 
 @Module({
-  imports: [GameModule, CoreModule],
+  imports: [
+    GameModule,
+    CoreModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'static'),
+      serveRoot: '/static',
+    }),
+  ],
   controllers: [],
   providers: [],
   exports: [],
